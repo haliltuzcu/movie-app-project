d4ec2bb8e4ea1e0011378d0ae07d7c13582033ca
haliltuzcu <007canberk@gmail.com>
2023-03-15 15:53:24 +0300

    google

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import AuthContextProvider from "./context/AuthContext";
 import AppRouter from "./router/AppRouter";
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from "react-toastify";
+import MovieContextProvider from "./context/MovieContext";
+
 const App = () => {
   return (
-    <div>
+    <div className="dark:bg-gray-dark-main">
       <AuthContextProvider>
-        <AppRouter />
-        <ToastContainer />
+        <MovieContextProvider>
+          <AppRouter />
+          <ToastContainer />
+        </MovieContextProvider>
       </AuthContextProvider>
     </div>
   );
